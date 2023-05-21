6d16709d609fb59e17c8487fc17a55124b2d2a8d
Emmanuel James <chiderajames55@gmail.com>
2023-05-21 21:58:00 +0100

    Print Linked lists

---
+++ C Programming Exercises, Practice & Solution/DSA/Linked-List/printLists.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node
+{
+	int data;
+    struct node *link;
+};
+
+void print_nodes(struct node *head);
+
+int main() {
+	struct node *head = NULL;
+
+	head = (struct node *)malloc(sizeof(struct node));
+	head->data = 1;
+	head->link = NULL;
+
+ 	struct node *current = (struct node *)malloc(sizeof(struct node));
+	current->data = 2;
+	current->link = NULL;
+
+	head->link = current;
+
+	print_nodes(head);
+
+}
+
+void print_nodes(struct node *head) {
+    if ( head == NULL ) 
+        printf("Linked List is empty");
+
+    struct node *ptr = head;
+
+    while ( ptr != NULL ) {
+        printf("@[%p] - %d\n", ptr, ptr->data);
+        ptr=ptr->link;
+    }
+}
