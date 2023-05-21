586b79167cd95833bc4a0c2e141398e3549b2053
Emmanuel James <chiderajames55@gmail.com>
2023-05-21 21:57:01 +0100

    Create Linked List

---
+++ C Programming Exercises, Practice & Solution/DSA/Linked-List/create.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node
+{
+	int data;
+    struct node *link;
+};
+
+
+int main() {
+	struct node *head = NULL;
+
+	head = (struct node *)malloc(sizeof(struct node));
+	head->data = 1;
+	head->link = NULL;
+
+ 	struct node *current = (struct node *)malloc(sizeof(struct node));
+	current->data = 2;
+	current->link = NULL;
+
+	head->link = current;
+}
